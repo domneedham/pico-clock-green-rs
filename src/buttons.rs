adb1332aa5201870e98115ed0dde75d6f7470766
Dom Needham <=>
2023-07-26 06:49:09 +0100

    Add docs to buttons module

--- src/buttons.rs
+++ src/buttons.rs
@@ -2,15 +2,31 @@ use embassy_rp::{gpio::Input, peripherals::*};
 use embassy_sync::{blocking_mutex::raw::ThreadModeRawMutex, signal::Signal};
 use embassy_time::{Duration, Instant, Timer};
 
+/// Type of button press made.
+/// Double clicks are not yet supported but planned.
 pub enum ButtonPress {
+    /// When the button click duration is <=500ms.
     ShortPress,
+
+    /// When the button click duration is >500ms.
     LongPress,
 }
 
+/// Signal for when the top button has been pressed.
 pub static BUTTON_ONE_PRESS: Signal<ThreadModeRawMutex, ButtonPress> = Signal::new();
+
+/// Signal for when the middle button has been pressed.
 pub static BUTTON_TWO_PRESS: Signal<ThreadModeRawMutex, ButtonPress> = Signal::new();
+
+/// Signal for when the bottom button has been pressed.
 pub static BUTTON_THREE_PRESS: Signal<ThreadModeRawMutex, ButtonPress> = Signal::new();
 
+/// Wait for changes async on the top button being pressed.
+///
+/// Will inform signal of button press after the full press has been completed.
+/// The type of press is recorded in the ButtonPress enum.
+///
+/// This task has no way of cancellation.
 #[embassy_executor::task]
 pub async fn button_one_task(mut button: Input<'static, PIN_2>) -> ! {
     loop {
@@ -32,6 +48,12 @@ pub async fn button_one_task(mut button: Input<'static, PIN_2>) -> ! {
     }
 }
 
+/// Wait for changes async on the middle button being pressed.
+///
+/// Will inform signal of button press after the full press has been completed.
+/// The type of press is recorded in the ButtonPress enum.
+///
+/// This task has no way of cancellation.
 #[embassy_executor::task]
 pub async fn button_two_task(mut button: Input<'static, PIN_17>) -> ! {
     loop {
@@ -53,6 +75,12 @@ pub async fn button_two_task(mut button: Input<'static, PIN_17>) -> ! {
     }
 }
 
+/// Wait for changes async on the bottom button being pressed.
+///
+/// Will inform signal of button press after the full press has been completed.
+/// The type of press is recorded in the ButtonPress enum.
+///
+/// This task has no way of cancellation.
 #[embassy_executor::task]
 pub async fn button_three_task(mut button: Input<'static, PIN_15>) -> ! {
     loop {
