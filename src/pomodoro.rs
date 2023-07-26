252908c4d35c0786335d77152d64f1fcc992d0d7
Dom Needham <=>
2023-07-26 06:42:48 +0100

    add docs for pomodoro app

--- src/pomodoro.rs
+++ src/pomodoro.rs
@@ -14,9 +14,11 @@ use crate::{
     display::display_matrix::DISPLAY_MATRIX,
 };
 
+/// Channel for firing events of when tasks should be stopped.
 static STOP_APP_CHANNEL: PubSubChannel<ThreadModeRawMutex, StopAppTasks, 1, 1, 1> =
     PubSubChannel::new();
 
+/// Depict the current running state of the pomodoro timer.
 #[derive(Clone, Copy)]
 enum RunningState {
     NotStarted,
@@ -25,6 +27,7 @@ enum RunningState {
     Finished,
 }
 
+/// Manage active state of the pomodoro app.
 struct PomoState {
     running: RunningState,
     minutes: u32,
@@ -32,6 +35,7 @@ struct PomoState {
 }
 
 impl PomoState {
+    /// Create a new pomodoro state with the set defaults.
     const fn new() -> Self {
         Self {
             running: RunningState::NotStarted,
@@ -40,6 +44,7 @@ impl PomoState {
         }
     }
 
+    /// Reset the pomodoro state to the defaults it initialises with.
     pub fn reset(&mut self) {
         self.minutes = 30;
         self.seconds = 0;
@@ -47,12 +52,16 @@ impl PomoState {
     }
 }
 
+/// Static reference to the pomo state so it can be accessed by static tasks.
 static POMO_STATE: Mutex<ThreadModeRawMutex, RefCell<PomoState>> =
     Mutex::new(RefCell::new(PomoState::new()));
 
+/// Pomodoro app.
+/// Allows for setting a time up to 60 minutes and counting down to 0 seconds.
 pub struct PomodoroApp {}
 
 impl PomodoroApp {
+    /// Create a new pomodoro app.
     pub fn new() -> Self {
         Self {}
     }
@@ -154,16 +163,19 @@ impl App for PomodoroApp {
     }
 }
 
+/// Get the running state value from the static pomodoro state.
 async fn get_running_state() -> RunningState {
     POMO_STATE.lock().await.borrow().running
 }
 
+/// Get the (minutes, seconds) state value from the static pomodoro state.
 async fn get_time() -> (u32, u32) {
     let minutes = POMO_STATE.lock().await.borrow().minutes;
     let seconds = POMO_STATE.lock().await.borrow().seconds;
     (minutes, seconds)
 }
 
+/// Set the new time to display and count down from on the static pomodoro state.
 async fn set_time(minutes: u32, seconds: u32) {
     let mut guard = POMO_STATE.lock().await;
     let state = guard.borrow_mut().get_mut();
@@ -172,6 +184,8 @@ async fn set_time(minutes: u32, seconds: u32) {
     state.seconds = seconds;
 }
 
+/// Set the running state on the static pomodoro state.
+/// Will show/hide the CountDown icon on the display depending on the state passed.
 async fn set_running(running: RunningState) {
     let mut guard = POMO_STATE.lock().await;
     let state = guard.borrow_mut().get_mut();
@@ -185,11 +199,15 @@ async fn set_running(running: RunningState) {
     }
 }
 
+/// Will show the time grabbed from the static pomodoro state.
 async fn show_time() {
     let (minutes, seconds) = get_time().await;
     DISPLAY_MATRIX.queue_time(minutes, seconds, 0, true).await;
 }
 
+/// The pomodoro countdown loop.
+///
+/// Will continue to run as long as the running state is running or paused.
 #[embassy_executor::task]
 async fn countdown() {
     let mut stop_task_sub = STOP_APP_CHANNEL.subscriber().unwrap();
